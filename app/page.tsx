645ea75b6d059eb6729ee74f4a604858d11f6cfb
chrisarevalo11 <christian.arevalo.fonseca@gmail.com>
2024-10-04 14:04:42 -0500

    chore: added tailwind and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-'use client';
+"use client";
 
 import { DynamicWidget } from "@/lib/dynamic";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import DynamicMethods from "@/app/components/Methods";
-import Image from 'next/image';
-import './page.css';
+import Image from "next/image";
 
 const checkIsDarkSchemePreferred = () => {
-  if (typeof window !== 'undefined') {
-    return window.matchMedia?.('(prefers-color-scheme:dark)')?.matches ?? false;
+  if (typeof window !== "undefined") {
+    return window.matchMedia?.("(prefers-color-scheme:dark)")?.matches ?? false;
   }
   return false;
 };
@@ -17,26 +16,50 @@ export default function Main() {
   const [isDarkMode, setIsDarkMode] = useState(checkIsDarkSchemePreferred);
 
   useEffect(() => {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
     const handleChange = () => setIsDarkMode(checkIsDarkSchemePreferred());
-    
-    darkModeMediaQuery.addEventListener('change', handleChange);
-    return () => darkModeMediaQuery.removeEventListener('change', handleChange);
+
+    darkModeMediaQuery.addEventListener("change", handleChange);
+    return () => darkModeMediaQuery.removeEventListener("change", handleChange);
   }, []);
 
   return (
-    <div className={`container ${isDarkMode ? 'dark' : 'light'}`}>
+    <div className={`container ${isDarkMode ? "dark" : "light"}`}>
       <div className="header">
         <Image
           className="logo"
           src={isDarkMode ? "/logo-light.png" : "/logo-dark.png"}
           alt="dynamic"
-          width={150} 
-          height={50} 
+          width={150}
+          height={50}
         />
         <div className="header-buttons">
-          <button className="docs-button" onClick={() => window.open('https://docs.dynamic.xyz', '_blank', 'noopener,noreferrer')}>Docs</button>
-          <button className="get-started" onClick={() => window.open('https://app.dynamic.xyz', '_blank', 'noopener,noreferrer')}>Get started</button>
+          <button
+            className="docs-button bg-black"
+            onClick={() =>
+              window.open(
+                "https://docs.dynamic.xyz",
+                "_blank",
+                "noopener,noreferrer"
+              )
+            }
+          >
+            Docs
+          </button>
+          <button
+            className="get-started"
+            onClick={() =>
+              window.open(
+                "https://app.dynamic.xyz",
+                "_blank",
+                "noopener,noreferrer"
+              )
+            }
+          >
+            Get started
+          </button>
         </div>
       </div>
       <div className="modal">
@@ -49,10 +72,10 @@ export default function Main() {
           className="footer-image"
           src={isDarkMode ? "/image-dark.png" : "/image-light.png"}
           alt="dynamic"
-          width={100} 
-          height={100} 
+          width={100}
+          height={100}
         />
       </div>
-    </div> 
+    </div>
   );
 }
